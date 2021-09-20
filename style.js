d047693445cd5aa08031761d6e5ba72ec0adbbfb
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2021-09-20 12:55:40 +0200

    Update style.js

--- style.js
+++ style.js
@@ -48,6 +48,7 @@
 				{ name: 'Lista Ordenada 6', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-6' } },
 				{ name: 'Lista Ordenada 7', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-7' } },
 				{ name: 'Lista Ordenada 8', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-8' } },
+				{ name: 'Lista Ordenada 9', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-9' } },
 
 				{ name: 'Lista Desordenada 1', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-1' } },
 				{ name: 'Lista Desordenada 2', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-2' } },
